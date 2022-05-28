0f986076382c68245361968591d82557656e9718
chengxx <cheeriocheng@gmail.com>
2022-05-27 19:02:28 -0500

    supports window resize

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import * as THREE from 'three'
 
 // init
 
+const scene = new THREE.Scene()
+scene.background = new THREE.Color( 0x8FBCD4 );
+
 const camera = new THREE.PerspectiveCamera( 
   70, window.innerWidth / window.innerHeight, 0.01, 10 
-)
-camera.position.z = 1
+);
+camera.position.z = 2;
+scene.add(camera);
 
-const scene = new THREE.Scene()
+// scene.add( new THREE.AmbientLight( 0x8FBCD4, 0.4 ) );
+// const pointLight = new THREE.PointLight( 0xffffff, 1 );
+// camera.add( pointLight );
 
 const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
 const material = new THREE.MeshNormalMaterial()
@@ -16,9 +22,12 @@ const mesh = new THREE.Mesh( geometry, material )
 scene.add( mesh )
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } )
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight )
 renderer.setAnimationLoop( animation )
 document.body.appendChild( renderer.domElement )
+
+window.addEventListener( 'resize', onWindowResize );
 
 // animation
 
@@ -28,5 +37,16 @@ function animation( time: number ) {
   mesh.rotation.y = time / 1000
 
   renderer.render( scene, camera )
+
+  
+}
+
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
