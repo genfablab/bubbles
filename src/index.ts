7d41ba2416f7b1536db090753710dd5d6ed861bd
chengxx <cheeriocheng@gmail.com>
2022-05-29 15:31:10 -0500

    extrusion with rounded edge

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,62 @@ import * as THREE from 'three'
 // init
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0x8FBCD4 );
+scene.background = new THREE.Color(0x8FBCD4);
 
-const camera = new THREE.PerspectiveCamera( 
-  70, window.innerWidth / window.innerHeight, 0.01, 10 
+const camera = new THREE.PerspectiveCamera(
+  70, window.innerWidth / window.innerHeight, 0.01, 50
 );
-camera.position.z = 2;
+camera.position.z = 30;
 scene.add(camera);
 
-// scene.add( new THREE.AmbientLight( 0x8FBCD4, 0.4 ) );
-// const pointLight = new THREE.PointLight( 0xffffff, 1 );
-// camera.add( pointLight );
+const mesh = genGeo();
+scene.add(mesh);
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
-const material = new THREE.MeshNormalMaterial()
 
-const mesh = new THREE.Mesh( geometry, material )
-scene.add( mesh )
+function genGeo() {
+  const length = 5, width = 3;
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } )
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight )
-renderer.setAnimationLoop( animation )
-document.body.appendChild( renderer.domElement )
+  const shape = new THREE.Shape();
+  shape.moveTo(0, 0);
+  shape.lineTo(0, width);
+  shape.lineTo(length, width);
+  shape.lineTo(length, 0);
+  shape.lineTo(0, 0);
 
-window.addEventListener( 'resize', onWindowResize );
+  const extrudeSettings = {
+    steps: 2,
+    depth: 0,
+    bevelEnabled: true,
+    bevelThickness: 1,
+    bevelSize: 1,
+    bevelOffset: 0,
+    bevelSegments: 5
+  };
+
+  const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshNormalMaterial();
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setAnimationLoop(animation)
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', onWindowResize);
 
 // animation
 
-function animation( time: number ) {
+function animation(time: number) {
 
-  mesh.rotation.x = time / 2000
+  // mesh.rotation.x = time / 2000
   mesh.rotation.y = time / 1000
 
-  renderer.render( scene, camera )
+  renderer.render(scene, camera)
 
-  
+
 }
 
 
@@ -47,6 +67,6 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
